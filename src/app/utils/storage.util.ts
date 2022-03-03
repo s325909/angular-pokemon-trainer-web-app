3b23887f26a40c1c3f61b0dea082e906d78dbf40
Jaso Cyril <33516374+s325909@users.noreply.github.com>
2022-03-03 15:38:08 +0100

    FEAT: Return undefined instead of null

--- src/app/utils/storage.util.ts
+++ src/app/utils/storage.util.ts
@@ -4,15 +4,15 @@ export class StorageUtil {
         sessionStorage.setItem(key, JSON.stringify(value));
     }
     
-    public static storageRead<T>(key: string): T | null {
+    public static storageRead<T>(key: string): T | undefined {
         const storedValue = sessionStorage.getItem(key);
         try {
             if (storedValue) return JSON.parse(storedValue) as T;
-            return null;
+            return undefined;
         } catch (error) {
             // Remove invalid data!
             sessionStorage.removeItem(key);
-            return null;
+            return undefined;
         }
     }
 }
