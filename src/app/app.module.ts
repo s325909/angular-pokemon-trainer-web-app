7a0fa2349104f57ad3da5141f355b4a10313f505
Jaso Cyril <33516374+s325909@users.noreply.github.com>
2022-03-03 13:16:11 +0100

    FEAT: Added routing to pages in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { CataloguePage } from './pages/catalogue/catalogue.page';
@@ -14,7 +15,8 @@ import { LandingPage } from './pages/landing/landing.page';
     LandingPage,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
