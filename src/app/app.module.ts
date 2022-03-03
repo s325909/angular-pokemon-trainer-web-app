08716d1278230db34ac0cdc32a8a03c882f28946
Jaso Cyril <33516374+s325909@users.noreply.github.com>
2022-03-03 13:00:18 +0100

    FEAT: Generated main components landing, trainer and catalogue
    Generatedat with ng g c path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { TrainerComponent } from './components/trainer/trainer.component';
+import { CatalogueComponent } from './components/catalogue/catalogue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    TrainerComponent,
+    CatalogueComponent
   ],
   imports: [
     BrowserModule
