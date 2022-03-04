710129ba1ea982dea3380ad7f790cb1390f4771f
AmmarAhmed007 <ammar.ahmed@no.experis.com>
2022-03-04 14:45:43 +0100

    Feat: Implemented catch pokemon button to the list.

--- src/app/components/catch-pokemon-button/catch-pokemon-button.component.ts
+++ src/app/components/catch-pokemon-button/catch-pokemon-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-catch-pokemon-button',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatchPokemonButtonComponent implements OnInit {
 
+  @Input() pokemonName: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onCatchClick(): void {
+    alert("You caught the pokemon: " + this.pokemonName + ". Congrats brother!")
   }
 
 }
