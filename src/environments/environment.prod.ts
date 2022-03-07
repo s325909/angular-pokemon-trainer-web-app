62c17a22fa001208f18f30a339c2733897b60638
s325909 <jaso_cyril@hotmail.com>
2022-03-07 20:37:05 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
+  apiTrainersKey: "q0oAROehq0u7HUS7yve0AQ==",
   apiTrainers: "https://aa-jc-vue-trivia-game.herokuapp.com/trainers",
   apiPokemons: "https://pokeapi.co/api/v2/pokemon",
+  urlPokemonImage: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
 };
