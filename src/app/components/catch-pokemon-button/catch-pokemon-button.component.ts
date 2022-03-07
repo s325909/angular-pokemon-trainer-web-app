75e365d07d29af384f3d0edce84de4883b4d3c8e
AmmarAhmed007 <ammar.ahmed@no.experis.com>
2022-03-07 12:21:03 +0100

    FEAT: Updated hasPokemon dynamically

--- src/app/components/catch-pokemon-button/catch-pokemon-button.component.ts
+++ src/app/components/catch-pokemon-button/catch-pokemon-button.component.ts
@@ -1,6 +1,8 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
+import { Trainer } from 'src/app/models/trainer.model';
 import { PokemonCatchService } from 'src/app/services/pokemon-catch.service';
+import { TrainerService } from 'src/app/services/trainer.service';
 
 @Component({
   selector: 'app-catch-pokemon-button',
@@ -9,25 +11,28 @@ import { PokemonCatchService } from 'src/app/services/pokemon-catch.service';
 })
 export class CatchPokemonButtonComponent implements OnInit {
 
+  public hasPokemon: boolean = false;
   @Input() pokemonName: string = "";
 
   get loading(): boolean {
     return this.pokemonCatchService.loading;
   }
 
-  constructor(private readonly pokemonCatchService: PokemonCatchService) { }
+  constructor(
+    private trainerService: TrainerService,
+    private readonly pokemonCatchService: PokemonCatchService) { }
 
   ngOnInit(): void {
+    this.hasPokemon = this.trainerService.inTrainerPokemon(this.pokemonName);
   }
 
   onCatchClick(): void {
     // Add pokemon to trainer pokemon
     this.pokemonCatchService.addToTrainerPokemon(this.pokemonName)
     .subscribe({
-      next: (response: any) => {
-        console.log("NEXT", response);
-        
-        
+      next: (response: Trainer) => {
+        // console.log("NEXT", response);
+        this.hasPokemon = this.trainerService.inTrainerPokemon(this.pokemonName);
       },
       error: (error: HttpErrorResponse) => {
         console.log("ERROR", error.message);
